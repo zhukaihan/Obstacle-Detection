f933e0487862a735e6cb07a36cc211a802d3421b
朱恺晗 <zhukaihan@foxmail.com>
2019-03-22 17:09:02 -0700

    Use landscape-only mode Use storyboard for UIImageViews Use png format to store captured rgb data and depth as alpha channel Use a separate thread for processing frames

--- Obstacle-Detection/ViewController.swift
+++ Obstacle-Detection/ViewController.swift
@@ -8,18 +8,19 @@
 
 import UIKit
 import AVFoundation
+import MobileCoreServices
 import Zip
 
 
 class ViewController: UIViewController, AVCaptureDepthDataOutputDelegate, AVCaptureVideoDataOutputSampleBufferDelegate, AVCaptureDataOutputSynchronizerDelegate {
     
     
-    
     let PHOTO_FOLDER_NAME = "Obstacle-Detection-imgs"
+    let FPS: Double = 15
     
     let captureSession = AVCaptureSession()
-    let depthView = UIImageView()
-    let previewView = UIImageView()
+    @IBOutlet var depthView: UIImageView!
+    @IBOutlet var previewView: UIImageView!
     let dataOutputQueue = DispatchQueue(label: "DepthQueue", qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem)
     let depthOutput = AVCaptureDepthDataOutput()
     let videoOutput = AVCaptureVideoDataOutput()
@@ -54,21 +55,47 @@ class ViewController: UIViewController, AVCaptureDepthDataOutputDelegate, AVCapt
         
     }
     
+    
     func initVideoCapture() {
-        
         
         self.captureSession.beginConfiguration()
         self.captureSession.sessionPreset = .photo
         
         // Confugure for dual cameras device.
-        let videoDevice = AVCaptureDevice.default(.builtInDualCamera,
-                                                  for: .video, position: .unspecified)
-        try? videoDevice?.lockForConfiguration()
-        videoDevice?.focusMode = .continuousAutoFocus
+        guard let videoDevice = AVCaptureDevice.default(.builtInDualCamera,
+                                                        for: .video, position: .unspecified) else { return }
+        try? videoDevice.lockForConfiguration()
+        
+        // Focus is automatic.
+        videoDevice.focusMode = .continuousAutoFocus
+        
+        // Set frame rate.
+        /*var lowestFormat: AVCaptureDevice.Format?
+        var lowestFrameRateRange: AVFrameRateRange?
+        
+        for format in videoDevice.formats {
+            for range in format.videoSupportedFrameRateRanges {
+                if lowestFrameRateRange == nil || range.minFrameRate < (lowestFrameRateRange?.minFrameRate)! {
+                    lowestFormat = format;
+                    lowestFrameRateRange = range;
+                    print(range.minFrameRate)
+                    print(range.maxFrameRate)
+                }
+            }
+        }
+        if lowestFormat != nil && lowestFrameRateRange != nil {
+            videoDevice.activeFormat = lowestFormat!;
+            videoDevice.activeVideoMinFrameDuration = CMTime(value: 1, timescale: 4)// lowestFrameRateRange!.maxFrameDuration
+            videoDevice.activeVideoMaxFrameDuration = CMTime(value: 1, timescale: 4)// lowestFrameRateRange!.maxFrameDuration
+            //videoDevice.activeDepthDataFormat = lowestFormat!;
+            //videoDevice.activeDepthDataMinFrameDuration = lowestFrameRateRange!.maxFrameDuration
+            print(lowestFrameRateRange?.minFrameDuration)
+            print(lowestFrameRateRange?.maxFrameDuration)
+        }*/
         
         // Add dual camera input to session.
         guard
-            let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice!),
+            let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice),
             self.captureSession.canAddInput(videoDeviceInput)
             else { return }
         self.captureSession.addInput(videoDeviceInput)
@@ -76,40 +103,38 @@ class ViewController: UIViewController, AVCaptureDepthDataOutputDelegate, AVCapt
         // Add depth data output to session.
         guard self.captureSession.canAddOutput(self.depthOutput) else { return }
         self.captureSession.addOutput(self.depthOutput)
-        if let connection = self.depthOutput.connections.first {
-            connection.videoOrientation = .landscapeRight
-        }
+        guard let connection = self.depthOutput.connections.first else { return }
+        connection.videoOrientation = .landscapeRight
         
         guard self.captureSession.canAddOutput(self.videoOutput) else {return}
         self.captureSession.addOutput(self.videoOutput)
-        if let connection = self.videoOutput.connections.first {
-            connection.videoOrientation = .landscapeRight
-        }
+        guard let videoConn = self.videoOutput.connections.first else { return }
+        videoConn.videoOrientation = .landscapeRight
         
         self.captureSync = AVCaptureDataOutputSynchronizer(dataOutputs: [videoOutput, depthOutput])
         self.captureSync?.setDelegate(self, queue: dataOutputQueue)
         
         self.captureSession.commitConfiguration()
         self.captureSession.startRunning()
-        videoDevice?.unlockForConfiguration()
+        videoDevice.unlockForConfiguration()
         
     }
+    
     
     func dataOutputSynchronizer(_ synchronizer: AVCaptureDataOutputSynchronizer, didOutput synchronizedDataCollection: AVCaptureSynchronizedDataCollection) {
         guard let depthData = synchronizedDataCollection[self.depthOutput] as? AVCaptureSynchronizedDepthData else {return}
         guard let imgData = synchronizedDataCollection[self.videoOutput] as? AVCaptureSynchronizedSampleBufferData else {return}
         
-        let depthDatMap = depthData.depthData.depthDataMap
-        let rawimage = CIImage(cvPixelBuffer: depthDatMap)
+        let depthDataMap = depthData.depthData.depthDataMap
+        let depthBuffer = CIImage(cvPixelBuffer: depthDataMap)
         
         // Convert depth data to an image to show.
-        let temporaryContext = CIContext()
-        guard let depthImg = temporaryContext.createCGImage(
-            rawimage,
+        guard let depthImg = CIContext().createCGImage(
+            depthBuffer,
             from: CGRect(
                 x: 0, y: 0,
-                width: CVPixelBufferGetWidth(depthDatMap),
-                height: CVPixelBufferGetHeight(depthDatMap)
+                width: CVPixelBufferGetWidth(depthDataMap),
+                height: CVPixelBufferGetHeight(depthDataMap)
             )
         ) else {return}
         
@@ -119,35 +144,35 @@ class ViewController: UIViewController, AVCaptureDepthDataOutputDelegate, AVCapt
         guard let videoImage = CIContext().createCGImage(ciImage, from: ciImage.extent) else { return }
         
         // Resize image to the same size as depth map.
-        let context = CGContext(
+        guard let imgContext = CGContext(
             data: nil,
             width: depthImg.width, height: depthImg.height,
             bitsPerComponent: depthImg.bitsPerComponent, bytesPerRow: depthImg.bytesPerRow,
             space: depthImg.colorSpace!, bitmapInfo: depthImg.bitmapInfo.rawValue
-        )
-        context!.interpolationQuality = .high
-        context!.draw(videoImage, in: CGRect(x: 0, y: 0, width: depthImg.width, height: depthImg.height))
-        let scaled = context?.makeImage()
+        ) else { return }
+        imgContext.interpolationQuality = .high
+        imgContext.draw(videoImage, in: CGRect(x: 0, y: 0, width: depthImg.width, height: depthImg.height))
+        guard let scaled = imgContext.makeImage() else { return }
         
         // Show imgs.
         self.showDepthImg(depthImg)
-        self.showVideoImg(scaled!)
+        self.showVideoImg(scaled)
     }
+    
     
     func showDepthImg(_ videoImage: CGImage) {
         DispatchQueue.main.async {
-            let image = UIImage(cgImage: videoImage, scale: 1.0, orientation: .right)
-            self.depthView.image = image
-            self.depthView.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: image.size)
+            self.depthView.image = UIImage(cgImage: videoImage)
         }
     }
     
+    
     func showVideoImg(_ videoImage: CGImage) {
         DispatchQueue.main.async {
-            self.previewView.image = UIImage(cgImage: videoImage, scale: 1.0, orientation: .right)
-            self.previewView.frame = CGRect(origin: CGPoint(x: 0, y: self.depthView.frame.maxY), size: (self.previewView.image?.size)!)
+            self.previewView.image = UIImage(cgImage: videoImage)
         }
     }
+    
     
     @IBAction func toggleDepthMode(_ sender: UISwitch) {
         self.depthOutput.isFilteringEnabled = sender.isOn
@@ -165,16 +190,10 @@ class ViewController: UIViewController, AVCaptureDepthDataOutputDelegate, AVCapt
     
     @IBAction func takePhoto(_ sender: UIButton) {
         do {
-            let photoFolder = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent(PHOTO_FOLDER_NAME)
-            try FileManager.default.createDirectory(at: photoFolder, withIntermediateDirectories: true, attributes: nil)
-            
-            let imgName = curTimeStamp()
-            
             guard let realImg = self.previewView.image?.cgImage else { return }
-            //guard let realData = realImg.pngData() else { return }
             guard let depthImg = self.depthView.image?.cgImage else { return }
-            //guard let depthData = depthImg.pngData() else { return }
             
+            // Settings for CGContext.
             let width = depthImg.width;
             let height = depthImg.height;
             
@@ -182,32 +201,37 @@ class ViewController: UIViewController, AVCaptureDepthDataOutputDelegate, AVCapt
             let bytesPerRow = bytesPerPixel * width;
             let bitsPerComponent = 8;
             
+            // Get the raw rgb data for real and depth imgs.
             let realContext = CGContext(data: nil, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: CGColorSpaceCreateDeviceRGB(), bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue | CGBitmapInfo.byteOrder32Big.rawValue)
             realContext?.draw(realImg, in: CGRect(x: 0, y: 0, width: width, height: height))
             
             let depthContext = CGContext(data: nil, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: CGColorSpaceCreateDeviceRGB(), bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue | CGBitmapInfo.byteOrder32Big.rawValue)
             depthContext?.draw(depthImg, in: CGRect(x: 0, y: 0, width: width, height: height))
             
+            // Set up pointer for
             guard let realData = realContext?.data?.bindMemory(to: UInt8.self, capacity: width * height) else { return }
             guard let depthData = depthContext?.data?.bindMemory(to: UInt8.self, capacity: width * height) else { return }
             
-            var base, offset:Int
-            
-            for y in 0..<height {
-                base = y * height * 4
-                for x in 0..<width {
-                    offset = base + x * 4
-                    realData[offset] = (depthData[offset] + depthData[offset + 1] + depthData[offset + 2]) / 3
+            // Traverse image data to set alpha for realData.
+            var offset = 0
+            for _ in 0..<height {
+                for _ in 0..<width {
+                    realData[offset + 3] = UInt8((Int(depthData[offset]) + Int(depthData[offset + 1]) + Int(depthData[offset + 2])) / 3)
+                    offset += 4
                 }
             }
             
+            // Store img to file.
+            let photoFolder = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent(PHOTO_FOLDER_NAME)
+            try FileManager.default.createDirectory(at: photoFolder, withIntermediateDirectories: true, attributes: nil)
             
-            do {
-                try realData.write(to: photoFolder.appendingPathComponent(imgName + "_realImg.png"))
-                try depthData.write(to: photoFolder.appendingPathComponent(imgName + "_depthImg.png"))
-            } catch {
-                print("shit happened when saving img" + error.localizedDescription)
-            }
+            let imgName = curTimeStamp()
+            
+            let imgPath = photoFolder.appendingPathComponent(imgName + ".png")
+            guard let dest = CGImageDestinationCreateWithURL(imgPath as CFURL, kUTTypePNG, 1, nil) else { return }
+            guard let imageToStore = realContext?.makeImage() else { return }
+            CGImageDestinationAddImage(dest, imageToStore, nil)
+            CGImageDestinationFinalize(dest)
             
         } catch {
             print("error in creating dir" + error.localizedDescription)
